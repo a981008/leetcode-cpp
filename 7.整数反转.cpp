b0cdc20b636c1e77583f50903c0ef0a4a56c26c4
2587450776 <2587450776>
2021-03-16 13:39:22 +0800

    0316

--- 7.整数反转.cpp
+++ 7.整数反转.cpp
@@ -4,13 +4,21 @@
  * [7] 整数反转
  * 
  * 注意题目标注不能使用 long，特判溢出
+ * c++/java 等高级语言中取余可正可负，数学中只能是正数
  */
 
 // @lc code=start
 class Solution {
 public:
     int reverse(int x) {
-        
+        int res = 0;
+        while (x) {
+            if (res > 0 && res > (INT_MAX - x % 10) / 10) return 0;
+            if (res < 0 && res < (INT_MIN - x % 10) / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res;
     }
 };
 // @lc code=end
