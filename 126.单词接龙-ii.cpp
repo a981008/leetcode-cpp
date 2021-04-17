d0eb760d43b12d9f1a85925f9c85879893981767
2587450776 <2587450776>
2021-04-17 17:07:20 +0800

    0417

---
+++ 126.单词接龙-ii.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=126 lang=cpp
+ *
+ * [126] 单词接龙 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    // 本质是一个图的最短路径问题
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+
+    }
+};
+// @lc code=end
+
